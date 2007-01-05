dcc8b55dbb445f0bb6b9fff1dddfe07eb448f069
woods <woods@da5c320b-a5ec-0310-b7f9-e4d854caa1e2>
2007-01-05 20:31:12 +0000

    Split out activerecord handling, so that I can debug it easier.
    git-svn-id: https://secure.westarete.com/svn/internal/plugins/validate_request/trunk@7742 da5c320b-a5ec-0310-b7f9-e4d854caa1e2

--- lib/abstract_params.rb
+++ lib/abstract_params.rb
@@ -1,4 +1,5 @@
 require 'parameter_pair'
+require 'activerecord_requirement'
 require 'request_error'
 
 module ValidateRequest
@@ -17,25 +18,31 @@ module ValidateRequest
         # Convert keys from symbols to strings, since that's how they appear
         # in the params hash.
         key = key.to_s
-
+        
+        # Look for this requirement in the given parameters. Let the child
+        # class (optional vs. required) tell us what to do if it's missing.
         value = parameters[key]
         if value.nil?
           next if skip_missing_parameter?(key)
         end
         
         # If the requirement is an ActiveRecord class, expand it into a 
-        # requirements hash of its content columns and their types.
-        if is_model? requirement
-          requirement = expand_active_record_to_hash_requirement(requirement)
+        # requirements hash of its content columns and their types. This 
+        # effectively simulates the user having specified all of the model's
+        # columns by hand using the standard hash notation.
+        if ActiveRecordRequirement.is_model? requirement
+          requirement = ActiveRecordRequirement.new(requirement).to_hash
         end
         
         if requirement.kind_of? Hash
+          # Recursively verify nested hashes.
           unless value.kind_of? Hash        
             raise RequestError, "parameter '#{key}' is not a compound value"
           end
           validate_and_delete!(requirement, value)
           parameters.delete(key) if value.empty?
         else
+          # Validate a normal key/value pair.
           ParameterPair.new(key, value).validate(requirement)
           parameters.delete(key)
         end
@@ -49,35 +56,6 @@ module ValidateRequest
     def skip_missing_parameter?(key)
       raise "not implemented"
     end    
-
-    private
-    
-    # Determine if the given requirement is an ActiveRecord model.
-    def is_model?(requirement)
-      requirement.respond_to? :ancestors and
-        requirement.ancestors.detect {|a| a == ActiveRecord::Base}
-    end
-    
-    # Pick out the desired content columns from the given activerecord class.
-    def validate_columns(klass)
-      ignore = %w( created_at updated_at created_on updated_on created_by updated_by )
-      columns = []
-      klass.content_columns.each do |column|
-        columns << column unless ignore.detect {|name| name == column.name }
-      end
-      columns
-    end
-
-    # Expand the given ActiveRecord::Base class into a requirements hash of 
-    # its content columns and their types.
-    def expand_active_record_to_hash_requirement(klass)
-      requirements = {}
-      validate_columns(klass).each do |column|
-        # For right now, we only support integer and text.
-        requirements[column.name] = (column.type == :integer) ? :integer : :text
-      end
-      requirements
-    end
     
   end
 end
---
+++ lib/activerecord_requirement.rb
@@ -0,0 +1,47 @@
+module ValidateRequest
+  # Represents a requirement whose type is an ActiveRecord class.
+  class ActiveRecordRequirement
+    attr_reader :requirements, :klass
+
+    IGNORE_COLUMNS = %w( created_at updated_at created_on updated_on created_by updated_by )
+
+    def initialize(klass)
+      @klass = klass
+      @columns = []
+      @requirements = {}
+      init_requirements
+    end
+    
+    # Determine if the given requirement is an ActiveRecord model.
+    def self.is_model?(requirement)
+      requirement.respond_to? :ancestors and
+        requirement.ancestors.detect {|a| a == ActiveRecord::Base}
+    end
+    
+    # Return the hash representation of the requirements for this ActiveRecord
+    # model.
+    def to_hash
+      @requirements
+    end
+    
+    private
+    
+    # Determine the hash representation of requirements for this ActiveRecord
+    # model.
+    def init_requirements
+      init_columns
+      @columns.each do |c|
+        # For right now, we only support integer and text.
+        @requirements[c.name] = (c.type == :integer) ? :integer : :text
+      end
+    end
+    
+    # Pick out the desired content columns for this activerecord class.
+    def init_columns
+      @klass.content_columns.each do |c|
+        @columns << c unless IGNORE_COLUMNS.detect {|name| name == c.name }
+      end
+    end
+
+  end
+end
