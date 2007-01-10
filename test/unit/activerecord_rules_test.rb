6e85df2357f2f7f4101c5d5f556d1e60011c73e5
woods <woods@da5c320b-a5ec-0310-b7f9-e4d854caa1e2>
2007-01-10 03:25:52 +0000

    Change variable names in param requirements from symbols to strings. This is more appropriate, and allows for the full scope of possible variable names, whereas symbols did not (only certain combinations of characters can be symbols).
    git-svn-id: https://secure.westarete.com/svn/internal/plugins/validate_request/trunk@7800 da5c320b-a5ec-0310-b7f9-e4d854caa1e2

--- test/unit/activerecord_rules_test.rb
+++ test/unit/activerecord_rules_test.rb
@@ -32,36 +32,36 @@ class ActiveRecordRulesTest < Test::Unit::TestCase
     
   def test_no_models
     assert_equal({}, ActiveRecordRules.new({}).expand)
-    assert_equal({:id => :integer}, ActiveRecordRules.new({:id => :integer}).expand)
-    assert_equal({:id => :integer, :name => {:first => :string}}, ActiveRecordRules.new({:id => :integer, :name => {:first => :string}}).expand)
+    assert_equal({"id" => :integer}, ActiveRecordRules.new({"id" => :integer}).expand)
+    assert_equal({"id" => :integer, "name" => {"first" => :string}}, ActiveRecordRules.new({"id" => :integer, "name" => {"first" => :string}}).expand)
   end
   
   def test_model
-    assert_equal({:dog=>{"name"=>:text, "breed"=>:text, "age_in_years"=>:integer}}, ActiveRecordRules.new({:dog => Dog}).expand)
-    assert_equal({:pound => {:dog=>{"name"=>:text, "breed"=>:text, "age_in_years"=>:integer}}}, ActiveRecordRules.new({:pound => {:dog => Dog}}).expand)
-    assert_equal({:dog1=>{"name"=>:text, "breed"=>:text, "age_in_years"=>:integer}, :dog2=>{"name"=>:text, "breed"=>:text, "age_in_years"=>:integer}}, ActiveRecordRules.new({:dog1 => Dog, :dog2 => Dog}).expand)
+    assert_equal({"dog"=>{"name"=>:text, "breed"=>:text, "age_in_years"=>:integer}}, ActiveRecordRules.new({"dog" => Dog}).expand)
+    assert_equal({"pound" => {"dog"=>{"name"=>:text, "breed"=>:text, "age_in_years"=>:integer}}}, ActiveRecordRules.new({"pound" => {"dog" => Dog}}).expand)
+    assert_equal({"dog1"=>{"name"=>:text, "breed"=>:text, "age_in_years"=>:integer}, "dog2"=>{"name"=>:text, "breed"=>:text, "age_in_years"=>:integer}}, ActiveRecordRules.new({"dog1" => Dog, "dog2" => Dog}).expand)
   end
 
   def test_model_with_except
-    assert_equal({:dog=>{"name"=>:text, "age_in_years"=>:integer}}, ActiveRecordRules.new({:dog => Dog, :except => :breed}).expand)
-    assert_equal({:dog=>{"breed"=>:text, "age_in_years"=>:integer}}, ActiveRecordRules.new({:dog => Dog, :except => :name}).expand)
+    assert_equal({"dog"=>{"name"=>:text, "age_in_years"=>:integer}}, ActiveRecordRules.new({"dog" => Dog, :except => "breed"}).expand)
+    assert_equal({"dog"=>{"breed"=>:text, "age_in_years"=>:integer}}, ActiveRecordRules.new({"dog" => Dog, :except => "name"}).expand)
   end 
   
   def test_model_with_multiple_excepts
-    assert_equal({:dog=>{"age_in_years"=>:integer}}, ActiveRecordRules.new({:dog => Dog, :except => [:name, :breed]}).expand)
+    assert_equal({"dog"=>{"age_in_years"=>:integer}}, ActiveRecordRules.new({"dog" => Dog, :except => ["name", "breed"]}).expand)
   end
   
   def test_different_models_with_different_excepts
-    assert_equal({:dog=>{"breed"=>:text, "age_in_years"=>:integer}, :nested => {:dog=>{"name"=>:text, "age_in_years"=>:integer}}}, ActiveRecordRules.new({:dog => Dog, :except => :name, :nested => {:dog => Dog, :except => :breed}}).expand)    
+    assert_equal({"dog"=>{"breed"=>:text, "age_in_years"=>:integer}, "nested" => {"dog"=>{"name"=>:text, "age_in_years"=>:integer}}}, ActiveRecordRules.new({"dog" => Dog, :except => "name", "nested" => {"dog" => Dog, :except => "breed"}}).expand)    
   end
   
   def test_nested_model_with_except
-    assert_equal({:pound => {:dog=>{"name"=>:text, "age_in_years"=>:integer}}}, ActiveRecordRules.new(:pound => {:dog => Dog, :except => :breed}).expand)
+    assert_equal({"pound" => {"dog"=>{"name"=>:text, "age_in_years"=>:integer}}}, ActiveRecordRules.new("pound" => {"dog" => Dog, :except => "breed"}).expand)
   end
   
   def test_except_without_model_should_be_treated_like_a_normal_argument
-    assert_equal({:id => :integer, :except => :text}, ActiveRecordRules.new({:id => :integer, :except => :text}).expand)
-    assert_equal({:id => :integer, :except => :text}, ActiveRecordRules.new({:id => :integer, :except => :text}).expand)
+    assert_equal({"id" => :integer, :except => :text}, ActiveRecordRules.new({"id" => :integer, :except => :text}).expand)
+    assert_equal({"id" => :integer, "except" => :text}, ActiveRecordRules.new({"id" => :integer, "except" => :text}).expand)
   end
   
 end
