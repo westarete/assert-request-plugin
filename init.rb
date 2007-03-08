805e6f3d0c77847f12f315dfb4310cdc5d47d6af
woods <woods@da5c320b-a5ec-0310-b7f9-e4d854caa1e2>
2007-03-08 18:24:36 +0000

    Found a way to have production-time handling of the exceptions done by default. No more need to use exception_notifier to do this. The plugin will automatically render and log the exceptions in development, but will log them and display the 404 page in production.
    git-svn-id: https://secure.westarete.com/svn/internal/plugins/validate_request/trunk@8080 da5c320b-a5ec-0310-b7f9-e4d854caa1e2

--- init.rb
+++ init.rb
@@ -1,1 +1,14 @@
-ActionController::Base.send :include, AssertRequest
+class ActionController::Base 
+  # Ensure that all controllers have direct access to assert_request.
+  include AssertRequest
+
+  # In production mode, trap assert_request's RequestError exceptions, and
+  # render a 404 response instead.
+  def rescue_action_in_public_with_request_error(exception)
+    if exception.kind_of? RequestError
+      render :file => "#{RAILS_ROOT}/public/404.html", :status => "404 Not Found"
+    end
+  end
+  alias_method_chain :rescue_action_in_public, :request_error
+  
+end
