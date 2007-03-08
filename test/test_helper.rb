5f60e49aa30c76f4856256da937bb0d63e185d8c
woods <woods@da5c320b-a5ec-0310-b7f9-e4d854caa1e2>
2007-03-08 04:55:20 +0000

    Move some stuff from the functional test into the general test helper so that the unit tests can get to it too.
    git-svn-id: https://secure.westarete.com/svn/internal/plugins/validate_request/trunk@8061 da5c320b-a5ec-0310-b7f9-e4d854caa1e2

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -31,3 +31,17 @@ class Test::Unit::TestCase
     flunk "Received a #{exception.to_s} exception, but wasn't expecting one: #{e}"
   end
 end
+
+# Simple model to use while testing ActiveRecord requirement types.
+class Dog < ActiveRecord::Base ; end
+
+# Redefine the "render" method to render nothing, so that we don't have
+# to create views for our actions or call "render :nothing => true" at the
+# end of each one. We can't do this with a filter, since calling render 
+# within a filter causes the action to be skipped.
+class ActionController::Base
+  def render_with_force_nothing
+    render_without_force_nothing :nothing => true
+  end    
+  alias_method_chain :render, :force_nothing
+end
