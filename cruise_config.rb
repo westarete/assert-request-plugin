fba48a5ec50796c0218a605b8f6de6f9a9abf3df
woods <woods@da5c320b-a5ec-0310-b7f9-e4d854caa1e2>
2007-03-28 02:20:13 +0000

    Add bill to recipient list for build failures.
    git-svn-id: https://secure.westarete.com/svn/internal/plugins/assert_request/trunk@8185 da5c320b-a5ec-0310-b7f9-e4d854caa1e2

--- cruise_config.rb
+++ cruise_config.rb
@@ -1,5 +1,5 @@
 # This is the configuration for cruise control, a continuous integration
 # tool.
 Project.configure do |project|
-  project.email_notifier.emails = %w( scott@westarete.com )
+  project.email_notifier.emails = %w( scott@westarete.com bill@westarete.com )
 end
