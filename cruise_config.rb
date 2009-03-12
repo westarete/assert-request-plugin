368034c3994c49f8ea90960ca415bb4f6f9964c6
Scott Woods <scott@westarete.com>
2009-03-12 07:27:03 -0400

    Don't need this cruise config anymore.

--- cruise_config.rb
+++
@@ -1,5 +0,0 @@
-# This is the configuration for cruise control, a continuous integration
-# tool.
-Project.configure do |project|
-  project.email_notifier.emails = %w( scott@westarete.com bill@westarete.com )
-end
